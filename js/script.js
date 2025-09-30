af0bf578bc978bc603b4586e3c505080e26b5bbe
Everither <everither.every@gmail.com>
2025-09-30 21:58:00 +1000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+clamp = (x, min, max) => { return Math.min(Math.max(x, min), max) }
+
+class Knob {
+    constructor(knobElement, min, max) {
+        this.knobElement = knobElement;
+        this.min = min;
+        this.max = max;
+
+        this.mouseDownY = 0;
+        this.currentRotation = 0;
+        this.mousePressed = false;
+
+        this.knobElement.addEventListener("mousedown", (e) => {
+            this.mousePressed = true;
+            this.mouseDownY = e.clientY;
+            this.currentRotation = parseInt(getComputedStyle(this.knobElement).getPropertyValue("--rotation"));
+        })
+
+        document.addEventListener("mouseup", () => {
+            this.mousePressed = false;
+        })
+
+        document.addEventListener("mousemove", (e) => {
+            if (!this.mousePressed) return;
+            let newRotation = clamp(this.mouseDownY - e.clientY + this.currentRotation, this.min, this.max);
+            this.knobElement.style.setProperty("--rotation", newRotation.toString() + "deg");
+        })
+    }
+}
+
+const knobElement1 = document.getElementsByClassName("knob")[0];
+const knobElement2 = document.getElementsByClassName("knob")[1];
+
+const knob1 = new Knob(knobElement1, -180, 180);
+const knob2 = new Knob(knobElement2, 0, 360);
+
