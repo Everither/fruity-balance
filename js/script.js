55f84414fc69c980467620e8459b160010f3d2e7
Everither <everither.every@gmail.com>
2025-10-03 21:06:21 +1000

    Add functionality to labels

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
+ratioToDB = (ratio) => { return 20*Math.log10(ratio) }
+
 clamp = (x, min, max) => { return Math.min(Math.max(x, min), max) }
 
 class Knob {
-    constructor(knobElement, min, max) {
+    constructor(knobElement, minDegree, maxDegree, minValue, maxValue, labelElement, labelFunc) {
         this.knobElement = knobElement;
-        this.min = min;
-        this.max = max;
+        this.minDegree = minDegree;
+        this.maxDegree = maxDegree;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        this.labelElement = labelElement;
+        this.labelFunc = labelFunc;
 
         this.mouseDownY = 0;
         this.currentRotation = 0;
@@ -28,15 +34,22 @@ class Knob {
 
         document.addEventListener("mousemove", (e) => {
             if (!this.mousePressed) return;
-            let newRotation = clamp(this.mouseDownY - e.clientY + this.currentRotation, this.min, this.max);
+            let newRotation = clamp(this.mouseDownY - e.clientY + this.currentRotation, this.minDegree, this.maxDegree);
             this.knobElement.style.setProperty("--rotation", newRotation.toString() + "deg");
+            this.labelElement.textContent = this.labelFunc(this.getValue(newRotation));
         })
+    }
+    getValue(rotation) {
+        return ((rotation - this.minDegree)/(this.maxDegree - this.minDegree)) * (this.maxValue - this.minValue) + this.minValue;
     }
 }
 
 const knobElement1 = document.getElementsByClassName("knob")[0];
 const knobElement2 = document.getElementsByClassName("knob")[1];
 
-const knob1 = new Knob(knobElement1, -180, 180);
-const knob2 = new Knob(knobElement2, 0, 360);
+const labelPan = document.getElementById("pan");
+const labelDB = document.getElementById("db");
+
+const knob1 = new Knob(knobElement1, -180, 180, -100, 100, labelPan, (x) => { return (x == 0) ? "Centered" : (x < 0) ? (-x).toFixed(0) + "% left" : x.toFixed(0) + "% right" });
+const knob2 = new Knob(knobElement2, 0, 360, 0, 1.33, labelDB, (x) => { return (x == 0) ? "-InfdB 0.00" : (20*Math.log10(x)).toFixed(1) + "dB " + x.toFixed(2) });
 
